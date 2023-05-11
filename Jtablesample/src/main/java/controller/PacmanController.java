0e2962d31353cbea7e838859d191de4e25e06447
micted <micdem94@gmail.com>
2023-05-11 03:50:05 +0200

    added continous movement for the pacman character

--- Jtablesample/src/main/java/controller/PacmanController.java
+++ Jtablesample/src/main/java/controller/PacmanController.java
@@ -1,21 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 import models.PacmanTableModel;
 
 public class PacmanController implements KeyListener {
-    
+
     private JTable table;
     private PacmanTableModel model;
     private int pacmanRow;
     private int pacmanCol;
-    
+    private Thread pacmanThread;
+
     public PacmanController(JTable table, PacmanTableModel model) {
         this.table = table;
         this.model = model;
@@ -29,53 +30,70 @@ public class PacmanController implements KeyListener {
                 }
             }
         }
+
+        pacmanThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    movePacman();
+                }
+            }
+        });
+        pacmanThread.start();
     }
-    
-    
-    
-    
+
+    private void movePacman() {
+        int currentRow = pacmanRow;
+        int currentCol = pacmanCol;
+        // determine the next cell to move to based on the direction of the last arrow key pressed
+        switch (lastKeyCode) {
+            case KeyEvent.VK_UP:
+                currentRow--;
+                break;
+            case KeyEvent.VK_DOWN:
+                currentRow++;
+                break;
+            case KeyEvent.VK_LEFT:
+                currentCol--;
+                break;
+            case KeyEvent.VK_RIGHT:
+                currentCol++;
+                break;
+            default:
+                return; // ignore other keys
+        }
+        // check if the new cell is within the bounds of the table
+        if (currentRow >= 0 && currentRow < model.getRowCount() && currentCol >= 0 && currentCol < model.getColumnCount()) {
+            // check if the new cell is already occupied by another Pacman image
+            if (model.getValueAt(currentRow, currentCol) == null) {
+                // move the Pacman image to the new cell
+                model.setValueAt(0, pacmanRow, pacmanCol); // erase the old Pacman image
+                model.setValueAt(1, currentRow, currentCol);
+                table.repaint(); // force table to redraw
+
+                // update the current position of the Pacman image
+                pacmanRow = currentRow;
+                pacmanCol = currentCol;
+            }
+        }
+    }
+
+    private int lastKeyCode = -1;
 
     @Override
     public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        int newRow = pacmanRow;
-        int newCol = pacmanCol;
-        // update the new row and column based on the arrow key pressed
-        switch (keyCode) {
-            case KeyEvent.VK_UP:
-                newRow--;
-                break;
-            case KeyEvent.VK_DOWN:
-                newRow++;
-                break;
-            case KeyEvent.VK_LEFT:
-                newCol--;
-                break;
-            case KeyEvent.VK_RIGHT:
-                newCol++;
-                break;
-            default:
-                return; // ignore other keys
-        }
-        // check if the new row and column are within the bounds of the table
-        if (newRow < 0 || newRow >= model.getRowCount() || newCol < 0 || newCol >= model.getColumnCount()) {
-            return; // ignore out-of-bounds moves
-        }
-        // check if the new cell is already occupied by another Pacman image
-        if (model.getValueAt(newRow, newCol) != null) {
-            return; // ignore overlapping moves
-        }
-        // move the Pacman image to the new cell and update the model
-        model.setValueAt(0, pacmanRow, pacmanCol);
-        model.setValueAt(1, newRow, newCol);
-        table.repaint(); // force table to redraw
-        pacmanRow = newRow;
-        pacmanCol = newCol;
+        lastKeyCode = e.getKeyCode();
     }
 
     @Override
     public void keyReleased(KeyEvent e) {}
+
 }
